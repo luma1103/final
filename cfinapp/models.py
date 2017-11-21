dde91dd01f0ee092cb4910ac43bec3e499e81fbb
luma1103 <31252219+luma1103@users.noreply.github.com>
2017-11-21 13:09:28 -0600

    Update models.py

--- cfinapp/models.py
+++ cfinapp/models.py
@@ -3,18 +3,10 @@ from django.utils import timezone
 
 # TAbla materia en la cual seran agregadas las nuevas materias
 class Materia(models.Model):
-    # Debe llevar campo nombre
-    Nombre = models.CharField(max_length=200)
+    # campos 
+    nombre = models.CharField(max_length=200)
+    catedratico = models.CharField(max_length=200)
     # una breve descripcion de la materia
     text = models.TextField()
-    created_date = models.DateTimeField(
-            default=timezone.now)
-    published_date = models.DateTimeField(
-            blank=True, null=True)
+    
 
-    def publish(self):
-        self.published_date = timezone.now()
-        self.save()
-
-    def __str__(self):
-        return self.nombre
